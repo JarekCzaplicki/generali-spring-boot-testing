217c01b5d57790f618279443c9d512753e9d969c
jarekczaplicki <jarekczaplicki@tlen.pl>
2023-12-08 13:37:27 +0100

    Init

---
+++ src/main/java/org/example/demogeneralispringboottesting/DemogeneraliSpringBootTestingApplication.java
@@ -0,0 +1,13 @@
+package org.example.demogeneralispringboottesting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemogeneraliSpringBootTestingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemogeneraliSpringBootTestingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/demogeneralispringboottesting/DemogeneraliSpringBootTestingApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.demogeneralispringboottesting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemogeneraliSpringBootTestingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
