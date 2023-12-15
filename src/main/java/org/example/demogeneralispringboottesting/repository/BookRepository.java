7b09886af00e836eb36fd31b014a4f0903dd7432
jarekczaplicki <jarekczaplicki@tlen.pl>
2023-12-15 15:46:17 +0100

    Kolejne testy

--- src/main/java/org/example/demogeneralispringboottesting/repository/BookRepository.java
+++ src/main/java/org/example/demogeneralispringboottesting/repository/BookRepository.java
@@ -2,11 +2,37 @@ package org.example.demogeneralispringboottesting.repository;
 
 import org.example.demogeneralispringboottesting.model.Book;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
+import java.math.BigDecimal;
 import java.util.List;
+import java.util.Optional;
 
 public interface BookRepository extends JpaRepository<Book, Long> {
     List<Book> findByGenre(String genre);
 
     List<Book> findBooksByAuthorAndPublishedYear(String author, int publishedYear);
+
+    int countBooksByPublishedYear(int year);
+
+    Optional<Book> findBookByIsbn(String isbn);
+
+    @Query(value = "SELECT * FROM books WHERE price < ?1", nativeQuery = true)
+    List<Book> findBooksCheaperThan(BigDecimal price);
+    List<Book> findByPriceLessThan(BigDecimal price);
+    List<Book> findAllByPriceBefore(BigDecimal price);
+
+    @Query(value = "SELECT * FROM books WHERE available = true", nativeQuery = true)
+    List<Book> findAllAvailableBooks();
+
+    List<Book> findBooksByAvailableTrue();
+
+    List<Book> findByAuthor(String author);
+
+    @Query(value = "SELECT * FROM books WHERE published_year >:year AND genre =:genre", nativeQuery = true)
+    List<Book> findRecentBooksByGenre(int year, String genre);
+
+    List<Book> findAllByPublishedYearAfterAndGenre(int year, String genre);
+
+    long countByGenre(String genre);
 }
--- src/test/java/org/example/demogeneralispringboottesting/repository/BookRepositoryTest.java
+++ src/test/java/org/example/demogeneralispringboottesting/repository/BookRepositoryTest.java
@@ -34,9 +34,10 @@ class BookRepositoryTest {
                 Arrays.asList(5, 4, 5, 4, 5)
         );
     }
+
     @DisplayName("Test zapisywania książki")
     @Test
-    void testSavedBook(){ // przemyśleć czemu nie przechodzi jako część wszystkich testów
+    void testSavedBook() { // przemyśleć czemu nie przechodzi jako część wszystkich testów
         // given
         // when
         Book savedBook = bookRepository.save(book);
@@ -48,9 +49,10 @@ class BookRepositoryTest {
 //        assertThat(savedBook).usingRecursiveComparison().ignoringCollectionOrder().isEqualTo(book);
         assertEquals(book, savedBook);
     }
+
     @DisplayName("Test wyszukiwania po Id")
     @Test
-    void findById(){
+    void findById() {
         // given
         Book savedBook = bookRepository.save(book);
         // when
@@ -60,9 +62,10 @@ class BookRepositoryTest {
         assertThat(foundBook).isPresent();
         assertThat(foundBook).isEqualTo(Optional.of(savedBook));
     }
+
     @DisplayName("Test aktualizacji danych książki")
     @Test
-    void testUpdateBook(){
+    void testUpdateBook() {
         // given
         Book savedBook = bookRepository.save(book);
         savedBook.setTitle("Zmieniony tytuł");
@@ -73,9 +76,10 @@ class BookRepositoryTest {
         // then
         assertThat(updatedBook.getTitle()).isEqualTo("Zmieniony tytuł");
     }
+
     @DisplayName("Test usuwania książki")
     @Test
-    void testDeleteBook(){
+    void testDeleteBook() {
         // given
         Book savedBook = bookRepository.save(book);
 
@@ -87,9 +91,10 @@ class BookRepositoryTest {
         assertThat(deletedBook).isNotPresent();
 
     }
+
     @DisplayName("Test wyszukiwania wszystkich książek")
     @Test
-    void testFindAllBooks(){
+    void testFindAllBooks() {
         // given
         bookRepository.save(book);
 
@@ -99,11 +104,12 @@ class BookRepositoryTest {
 
         // then
         assertThat(books).isNotNull();
-        assertThat(books.size()).isBetween(0,2);
+        assertThat(books.size()).isBetween(0, 2);
     }
+
     @DisplayName("Test wyszukiwania książek po gatunku")
     @Test
-    void testFindByGenre(){
+    void testFindByGenre() {
         // given
         String genre = bookRepository.save(book).getGenre();
 
@@ -113,9 +119,10 @@ class BookRepositoryTest {
         // then
         assertThat(books).isNotNull();
     }
+
     @DisplayName("Test wyszukiwania książek po autorze i roku - pozytywny scenariusz")
     @Test
-    void testFindByAuthorAndPublishedYearPositiveScenario(){
+    void testFindByAuthorAndPublishedYearPositiveScenario() {
         // given
         Book saved = bookRepository.save(book);
         String author = saved.getAuthor();
@@ -125,15 +132,15 @@ class BookRepositoryTest {
         List<Book> books = bookRepository.findBooksByAuthorAndPublishedYear(author, publishedYear);
 
         // then
-        assertThat(books).isNotNull();
+        assertThat(books).isNotEmpty();
         assertThat(books).allMatch(b -> b.getAuthor().equals(author) && b.getPublishedYear() == publishedYear);
     }
 
     @DisplayName("Test wyszukiwania książek po autorze i roku - negatywny scenariusz")
     @Test
-    void testFindByAuthorAndPublishedYearNegativeScenario(){
+    void testFindByAuthorAndPublishedYearNegativeScenario() {
         // given
-        Book saved = bookRepository.save(book);
+        bookRepository.save(book);
         String author = "Inny autor";
         int publishedYear = 1000;
 
@@ -143,14 +150,250 @@ class BookRepositoryTest {
         // then
         assertThat(books).isEmpty();
     }
-    //- Test liczenia książek wydanych w danym roku
-    //- Test wyszukiwania książek po ISBN
-    //- Test wyszukiwania książek tańszych od podanej ceny
-    //- Test wyszukiwania dostępnych książek
-    //- Test wyszukiwania książek po autorze (Native SQL)
-    //- Test wyszukiwania niedawno wydanych książek w danym gatunku
-    //- Test zliczania wszystkich książek
-    //- Test zliczania wszystkich książek w danym gatunku
+
+    @DisplayName("Test liczenia książek wydanych w danym roku")
+    @Test
+    void testCountBooksByPublishedYear() {
+        // given
+//        Book newBook = new Book(
+//                2L,
+//                "Hobbit",
+//                "J.R.R Tolkien",
+//                "1234567890000",
+//                1939,
+//                "Fantasy",
+//                new BigDecimal("49.99"),
+//                true,
+//                Arrays.asList(5, 4, 5, 4, 5));
+        bookRepository.save(book);
+//        bookRepository.save(newBook);
+        int year = 1954;
+
+        // when
+        int count = bookRepository.countBooksByPublishedYear(year);
+
+//        System.out.println(count);
+        // then
+        assertThat(count).isGreaterThan(0);
+    }
+
+    @DisplayName("Test wyszukiwania książki po ISBN")
+    @Test
+    void testFindBookByISBN(){
+        // given
+        bookRepository.save(book);
+        String isbn = "1234567890123";
+
+        // when
+        Book findedBook = bookRepository.findBookByIsbn(isbn).get();
+
+        // then
+        assertThat(findedBook).isNotNull();
+        assertThat(findedBook.getIsbn()).isEqualTo(isbn);
+    }
+
+    @DisplayName("Test wyszukiwania książek tańszych od podanej ceny v1")
+    @Test
+    void testFindBooksCheaperThan(){
+        // given
+        bookRepository.save(book);
+        BigDecimal price = new BigDecimal("50.00");
+
+        // when
+        List<Book> books = bookRepository.findBooksCheaperThan(price);
+
+        // then
+        assertThat(books).isNotEmpty();
+        assertThat(books).allMatch(b -> b.getPrice().compareTo(price) < 0);
+    }
+
+    @Test
+    @DisplayName("Test wyszukiwania książek tańszych od podanej ceny v2")
+    void testFindByPriceLessThan(){
+        // given
+        bookRepository.save(book);
+        BigDecimal price = new BigDecimal("50.00");
+
+        // when
+        List<Book> books = bookRepository.findByPriceLessThan(price);
+
+        // then
+        assertThat(books).isNotEmpty();
+        assertThat(books).allMatch(b -> b.getPrice().compareTo(price) < 0);
+    }
+
+    @Test
+    @DisplayName("Test wyszukiwania książek tańszych od podanej ceny v3")
+    void testFindAllByPriceBefore(){
+        // given
+        bookRepository.save(book);
+        BigDecimal price = new BigDecimal("50.00");
+
+        // when
+        List<Book> books = bookRepository.findAllByPriceBefore(price);
+
+        // then
+        assertThat(books).isNotEmpty();
+        assertThat(books).allMatch(b -> b.getPrice().compareTo(price) < 0);
+    }
+
+
+
+    @DisplayName("Test wyszukiwania dostępnych książek v2")
+    @Test
+    void testFindBooksByAvailableTrue(){
+        // given
+        Book newBook = new Book(
+                2L,
+                "Hobbit",
+                "J.R.R Tolkien",
+                "1234567890000",
+                1939,
+                "Fantasy",
+                new BigDecimal("49.99"),
+                true,
+                Arrays.asList(5, 4, 5, 4, 5));
+        bookRepository.save(book);
+        bookRepository.save(newBook);
+
+        // when
+        List<Book> books = bookRepository.findAllAvailableBooks();
+
+        // then
+        assertThat(books).isNotEmpty();
+        assertThat(books).allMatch(Book::getAvailable);
+    }
+
+    @DisplayName("Test wyszukiwania dostępnych książek")
+    @Test
+    void testFindAllAvailableBooks(){
+        // given
+        Book newBook = new Book(
+                2L,
+                "Hobbit",
+                "J.R.R Tolkien",
+                "1234567890000",
+                1939,
+                "Fantasy",
+                new BigDecimal("49.99"),
+                true,
+                Arrays.asList(5, 4, 5, 4, 5));
+        bookRepository.save(book);
+        bookRepository.save(newBook);
+
+        // when
+        List<Book> books = bookRepository.findBooksByAvailableTrue();
+
+        // then
+        assertThat(books).isNotEmpty();
+        assertThat(books).allMatch(Book::getAvailable);
+    }
+    @DisplayName("Test wyszukiwania książek po autorze")
+    @Test
+    void testFindByAuthorSQL(){
+        // given
+        Book newBook = new Book(
+                2L,
+                "Harry Potter",
+                "J.K. Rowling",
+                "1234567890010",
+                2001,
+                "Fantasy",
+                new BigDecimal("29.99"),
+                true,
+                Arrays.asList(5, 4, 5, 4, 5));
+        bookRepository.save(book);
+        bookRepository.save(newBook);
+        String author = newBook.getAuthor();
+
+        // when
+        List<Book> books = bookRepository.findByAuthor(author);
+
+
+        // then
+        assertThat(books).isNotEmpty();
+        assertThat(books).allMatch(b -> b.getAuthor().equals(author)); // Predykat zwraca zawsze 'True' dla pustego zbioru 'prawo pustego zbioru'
+    }
+    @DisplayName("Test wyszukiwania niedawno wydanych książek w danym gatunku")
+    @Test
+    void testFindRecentBooksByGenre(){
+// given
+        Book newBook = new Book(
+                2L,
+                "Harry Potter",
+                "J.K. Rowling",
+                "1234567890010",
+                2001,
+                "Fantasy",
+                new BigDecimal("29.99"),
+                true,
+                Arrays.asList(5, 4, 5, 4, 5));
+        bookRepository.save(book);
+        bookRepository.save(newBook);
+        int year = 1000;
+        String genre = "Fantasy";
+
+        // when
+        List<Book> books = bookRepository.findRecentBooksByGenre(year, genre);
+
+        // then
+        assertThat(books).isNotEmpty();
+        assertThat(books).allMatch(b -> b.getPublishedYear() > year && b.getGenre().equals(genre));
+        assertThat(books.size()).isEqualTo(2);
+    }
+
+    @DisplayName("Test wyszukiwania niedawno wydanych książek w danym gatunku")
+    @Test
+    void testFindAllByPublishedYearAfterAndGenre(){
+        // given
+        Book newBook = new Book(
+                2L,
+                "Harry Potter",
+                "J.K. Rowling",
+                "1234567890010",
+                2001,
+                "Fantasy",
+                new BigDecimal("29.99"),
+                true,
+                Arrays.asList(5, 4, 5, 4, 5));
+        bookRepository.save(book);
+        bookRepository.save(newBook);
+        int year = 1000;
+        String genre = "Fantasy";
+
+        // when
+        List<Book> books = bookRepository.findAllByPublishedYearAfterAndGenre(year, genre);
+
+        // then
+        assertThat(books).isNotEmpty();
+        assertThat(books).allMatch(b -> b.getPublishedYear() > year && b.getGenre().equals(genre));
+        assertThat(books.size()).isEqualTo(2);
+    }
+
+    @DisplayName("Test zliczania wszystkich książek")
+    @Test
+    void testCountAllBooks(){
+        // given
+        bookRepository.save(book);
+
+        // when
+        long count = bookRepository.count();
+
+        // then
+        assertThat(count).isGreaterThan(0);
+    }
+
+    @DisplayName("Test zliczania wszystkich książek w danym gatunku")
+    @Test
+    void testCountingBooksByGenre(){
+        // given
+        Book saved = bookRepository.save(book);
+
+        // when
+        long count = bookRepository.countByGenre(saved.getGenre());
+
+        assertThat(count).isEqualTo(1);
+    }
     //- Test wyszukiwania najdroższej książki
     //- Test wyszukiwania książek z ocenami powyżej określonego progu
     //- Test aktualizacji dostępności książki
