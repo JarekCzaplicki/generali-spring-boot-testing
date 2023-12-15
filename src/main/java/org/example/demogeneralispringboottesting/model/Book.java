e2cba18da176180f832982072f296328ab8f75e4
jarekczaplicki <jarekczaplicki@tlen.pl>
2023-12-15 10:23:06 +0100

    Po stworzeniu encji

---
+++ src/main/java/org/example/demogeneralispringboottesting/model/Book.java
@@ -0,0 +1,49 @@
+package org.example.demogeneralispringboottesting.model;
+
+import jakarta.annotation.Nonnull;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import lombok.*;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+
+@Entity
+@Table(name = "books")
+public class Book {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @NotNull
+    @Column(nullable = false)
+    private String title;
+    @NotNull
+    @Column(nullable = false)
+    private String author;
+    @NotNull
+    @Column(nullable = false, unique = true)
+    private String isbn;
+    @Column(name = "published_year",nullable = false)
+    private int publishedYear;
+    @NotNull
+    @Column(nullable = false)
+    private String genre;
+    @NotNull
+    @Column(nullable = false)
+    private BigDecimal price;
+    @NotNull
+    @Column(nullable = false)
+    private Boolean available;
+
+    @ElementCollection
+    @CollectionTable(name = "book_ratings", joinColumns = @JoinColumn(name = "book_id"))
+    @Column(name = "rating")
+    private List<Integer> ratings;
+
+
+}
